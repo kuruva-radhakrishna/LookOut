49a4282a06b7e712c8f058ea5c63a0dceda7c4bf
kuruva-radhakrishna <kuruvaradhakrishna77@gmail.com>
2024-10-13 10:16:04 +0530

    port-binding

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV != "production"){
 
 
 const express = require("express");
-const port = 3000;
+const port = process.env.PORT || 4000;
 const path = require("path");
 const methodOverride = require("method-override");
 const app = express();
